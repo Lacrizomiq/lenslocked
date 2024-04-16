cd1fc0f327013ebf5715b77db34123702c3a9319
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-04-16 22:33:43 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>404</h1><p>Page not found</p>")
+	}
+}
+
+func main() {
+  // http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+  fmt.Println("Starting the server on :3000...")
+  http.ListenAndServe(":3000", nil)
+}
