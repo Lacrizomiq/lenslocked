d8ef50013af287ce85d2346faa96cd028dcb4b54
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-04-20 15:28:39 +0200

    Mise à jour de main.go

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ Pages</h1>")
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 		case "/":
@@ -34,6 +39,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
