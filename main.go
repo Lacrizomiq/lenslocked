64df09dab2e1a2dc89c052cc131afe5f9b112981
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-04-20 15:39:52 +0200

    setting up chi routing

--- main.go
+++ main.go
@@ -22,35 +22,14 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>FAQ Pages</h1>")
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 		case "/":
-// 			homeHandler(w, r)
-// 		case "/contact":
-// 			contactHandler(w, r)
-// 		default:
-// 			http.Error(w, "Page not found", http.StatusNotFound)
-// 	}
-// }
-
-type Router struct {}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
 func main() {
 	r:= chi.NewRouter()
-	var router Router
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
   fmt.Println("Starting the server on :3000...")
-  http.ListenAndServe(":3000", router)
+  http.ListenAndServe(":3000", r)
 }
