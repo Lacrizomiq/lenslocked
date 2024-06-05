9ee2e5eaeebce76642f7bab97e8404cab12d6365
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-06-05 22:59:37 +0200

    restructuring the way my templates are parsed

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -6,59 +6,59 @@ import (
 )
 
 type User struct {
-    Name string
-    Bio  string
-    Age  int
-    City string
-		Attributes map[string]string
+	Name       string
+	Bio        string
+	Age        int
+	City       string
+	Attributes map[string]string
 }
 
 func main() {
-    t, err := template.ParseFiles("hello.gohtml")
-    if err != nil {
-        panic(err)
-    }
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
 
-    users := []User{
-			{
-					Name: "John Doe",
-					Bio: "Some bio",
-					Age: 25,
-					City: "San Francisco",
-					Attributes: map[string]string{
-						"Hobbies" : "Working out",
-						"Skills" : "Go, JavaScript",
+	users := []User{
+		{
+			Name: "John Doe",
+			Bio:  "Some bio",
+			Age:  25,
+			City: "San Francisco",
+			Attributes: map[string]string{
+				"Hobbies": "Working out",
+				"Skills":  "Go, JavaScript",
 			},
+		},
+		{
+			Name: "Jane Doe",
+			Bio:  "Another bio",
+			Age:  28,
+			City: "New York",
+			Attributes: map[string]string{
+				"Hobbies": "Reading",
+				"Skills":  "Python, Ruby",
 			},
-			{
-					Name: "Jane Doe",
-					Bio: "Another bio",
-					Age: 28,
-					City: "New York",
-					Attributes: map[string]string{
-						"Hobbies": "Reading",
-						"Skills": "Python, Ruby",
-					},
+		},
+		{
+			Name: "Michael Smith",
+			Bio:  "Yet another bio",
+			Age:  30,
+			City: "Los Angeles",
+			Attributes: map[string]string{
+				"Hobbies": "Swimming",
+				"Skills":  "Java, C",
 			},
-			{
-					Name: "Michael Smith",	
-					Bio: "Yet another bio",
-					Age: 30,
-					City: "Los Angeles",
-					Attributes: map[string]string{
-						"Hobbies": "Swimming",
-						"Skills": "Java, C",
-					},
-			},
-			}
-		
-		data := map[string]interface{}{
-			"Users": users,
-			"Header": "List of users",
-		}
+		},
+	}
 
-    err = t.Execute(os.Stdout, data)
-    if err != nil {
-        panic(err)
-    }
+	data := map[string]interface{}{
+		"Users":  users,
+		"Header": "List of users",
+	}
+
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
 }
--- main.go
+++ main.go
@@ -2,46 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joncalhoun/lenslocked/controllers"
 	"github.com/joncalhoun/lenslocked/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	tpl, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("Error loading template: %v", err)
-		http.Error(w, "Error loading template", http.StatusInternalServerError)
-		return
-	}
-	tpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath)
-}
-
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-			http.Error(w, "Page not found", http.StatusNotFound)
+		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
