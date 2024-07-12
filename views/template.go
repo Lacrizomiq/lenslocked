03a7f32c941a877802779d3bb4a042441627d529
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-07-12 23:14:30 +0200

    Template Function Errors and Buffer introduction

--- views/template.go
+++ views/template.go
@@ -1,8 +1,10 @@
 package views
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
@@ -22,13 +24,12 @@ func Must(t Template, err error) Template {
 // ParseFS parses the provided patterns from the provided fs.FS and returns a Template.
 // If there is an error parsing the template, an error is returned.
 func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
-	// Create a new template and add a function to generate a CSRF field.
-	// This is a contrived example to show how to add functions to templates.
+	// Create a new template and parse the provided patterns.
 	tpl := template.New(pattern[0])
 	tpl = tpl.Funcs(
 		template.FuncMap{
-			"csrfField": func() template.HTML {
-				return `<input type="hidden" />`
+			"csrfField": func() (template.HTML, error) {
+				return "", fmt.Errorf("csrfField is not implemented")
 			},
 		},
 	)
@@ -41,18 +42,6 @@ func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
 		htmlTpl: tpl,
 	}, nil
 }
-
-// Parse parses the provided file and returns a Template.
-// If there is an error parsing the template, an error is returned.
-// func Parse(filepath string) (Template, error) {
-// 	htmlTpl, err := template.ParseFiles(filepath)
-// 	if err != nil {
-// 		return Template{}, fmt.Errorf("parsing template: %w", err)
-// 	}
-// 	return Template{
-// 		htmlTpl: htmlTpl,
-// 	}, nil
-// }
 
 // Template is a wrapper around html/template to provide additional helper methods.
 type Template struct {
@@ -68,6 +57,7 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 		http.Error(w, "There was an error processing your request.", http.StatusInternalServerError)
 		return
 	}
+	// Add the csrfField function to the template.
 	tpl = tpl.Funcs(template.FuncMap{
 		"csrfField": func() template.HTML {
 			return csrf.TemplateField(r)
@@ -75,10 +65,14 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 	},
 	)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err = tpl.Execute(w, data)
+	// Create a buffer to write the template output to.
+	// If there is an error writing the template output, an error is logged and an http.Error is written to w.
+	var buf bytes.Buffer
+	err = tpl.Execute(&buf, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
 		return
 	}
+	io.Copy(w, &buf)
 }
