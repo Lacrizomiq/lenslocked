b128c8db30d4d0fc295dcb09688f359d8385ed18
Lacrizomiq <118858104+Lacrizomiq@users.noreply.github.com>
2024-07-15 13:31:36 +0200

    Defining session type and services in my session model

--- models/session.go
+++ models/session.go
@@ -1,7 +1,28 @@
 package models
 
+import "database/sql"
+
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is only set when creating a new session.
+	// We will use this to store the session token in the database.
+	// This will be left empty, as i only gonna store the hash of a session token.
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// Todo : Create the session token
+	// Todo : Implement SessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// Todo : Implement SessionService.User
+	return nil, nil
 }
